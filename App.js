9918cd89561b4c909318e80c982c5361b43ee059
Ernesto R. C. Pereda <ercp42@gmail.com>
2017-10-21 17:55:44 -0300

    Create content with a ScrollView

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import Accordion from '@ercpereda/react-native-accordion';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginTop: 20,
+  },
+  contentText: {
+    paddingTop: 15,
+    paddingRight: 15,
+    paddingBottom: 15,
+    paddingLeft: 15,
+    color: '#fff',
+  },
+});
 
 const Header = ({ isOpen }) =>
   <View style={{
@@ -16,20 +33,32 @@ const Header = ({ isOpen }) =>
     </View>;
 
 const Content = (
-  <View style={{
+  <ScrollView style={{
       display: 'flex',
-      backgroundColor: '#31363D'
+      backgroundColor: '#31363D',
+      height: 300,
     }}>
-      <Text style={{
-        paddingTop: 15,
-        paddingRight: 15,
-        paddingBottom: 15,
-        paddingLeft: 15,
-        color: '#fff',
-      }}>
-        This content is hidden in the accordion
-      </Text>
-    </View>);
+      <Text style={styles.contentText}>Item 1</Text> 
+      <Text style={styles.contentText}>Item 2</Text> 
+      <Text style={styles.contentText}>Item 3</Text> 
+      <Text style={styles.contentText}>Item 4</Text> 
+      <Text style={styles.contentText}>Item 5</Text> 
+      <Text style={styles.contentText}>Item 6</Text> 
+      <Text style={styles.contentText}>Item 7</Text> 
+      <Text style={styles.contentText}>Item 8</Text> 
+      <Text style={styles.contentText}>Item 9</Text> 
+      <Text style={styles.contentText}>Item 10</Text> 
+      <Text style={styles.contentText}>Item 11</Text> 
+      <Text style={styles.contentText}>Item 12</Text> 
+      <Text style={styles.contentText}>Item 13</Text> 
+      <Text style={styles.contentText}>Item 14</Text> 
+      <Text style={styles.contentText}>Item 15</Text> 
+      <Text style={styles.contentText}>Item 16</Text> 
+      <Text style={styles.contentText}>Item 17</Text> 
+      <Text style={styles.contentText}>Item 18</Text> 
+      <Text style={styles.contentText}>Item 19</Text> 
+      <Text style={styles.contentText}>Item 20</Text> 
+    </ScrollView>);
 
 export default class App extends React.Component {
   render() {
@@ -41,16 +70,15 @@ export default class App extends React.Component {
           duration={300}
           easing="easeOutCubic"
         />
+        <Accordion
+          header={Header}
+          content={Content}
+          duration={300}
+          easing="easeOutCubic"
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
